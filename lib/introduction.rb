d47e946a49ef0db676190e3b633cdd280869194a
Cristian <cpintado17@gmail.com>
2020-01-30 01:43:31 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,4 +1,4 @@
 def introduction_with_language(name, language)
-  phrease = name, language
+  phrase = name, language
   puts "Hello, my name is #{name}, I am learning to program in #{language}."
 end
