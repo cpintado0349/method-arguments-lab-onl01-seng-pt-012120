bf5a3c3f3f3295b5008061f5c2a118cb18a5902d
Cristian <cpintado17@gmail.com>
2020-01-30 01:27:18 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,4 +1,4 @@
 def introduction_with_language(name, language)
   puts "Hello, my name is #{name}, I am learning to program in #{language}."
-  introduction_with_language("Cristian", "Ruby")
+  introduction_with_language("name", "Ruby")
 end
